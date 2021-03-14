ecc2077feea0a390e04bfb83385f1239e1cb7483
jamesjbot <jongs.j@gmail.com>
2021-03-14 14:30:54 -0400

    chore: Remove old test code from pixabay.js

--- src/server/pixabay.js
+++ src/server/pixabay.js
@@ -16,42 +16,8 @@ function fetchPixabayImage(input) {
   let pixabayURL = `${pixabayBaseURL}?key=${process.env.PIXABAY_API_KEY}&q=${searchText}&image_type=photo`;
   console.log('Searching pixabay @',pixabayURL);
   return fetch(pixabayURL);
-  //   .then(data => data.json())
-  //   .then(response => {
-  //   console.log("parsed hello received response from pixabay");
-  //   console.log(response);
-  //   if (response.total != 0) {
-  //     console.log(response.hits[0].previewURL);
-  //   } else {
-  //     console.log('The response had zero hits');
-  //   }
-    //return response;
-  //});
 }
 
-// async function getImages(inputText, response) {
-//     console.log('pixabay.js getImages called');
-//     const jsonText = {data: inputText};
-//     console.log(`Your trying to get images with ${inputText}`);
-//     return fetch('http://localhost:8889/weather', {
-//       method: 'POST',
-//       credentials: 'same-origin',
-//       headers: {'Content-Type': 'application/json'},
-//       body: JSON.stringify(jsonText)
-//     })
-//     .then(res => res.json()) // Receive JSON String, convert to JSON
-//     .then(json => {response.send(json);})
-//     .catch(function(error) {
-//       console.log('Sorry error with getting sentiment',error);
-//     });
-// }
-
-// function test() {
-//   console.log('Test from pixabay');
-// }
-
 module.exports = {
-  test,
   fetchPixabayImage
 };
-//exports.test = test;
