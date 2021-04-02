ac2fd27e2abccdb217350744328973d541d8ff39
jamesjbot <jongs.j@gmail.com>
2021-04-01 22:41:26 -0400

    chore: remove console messages

--- src/server/pixabay.js
+++ src/server/pixabay.js
@@ -8,13 +8,10 @@ const fetch = require("node-fetch");
 
 
 function fetchPixabayImage(input) {
-  console.log('pixabay.js fetchPixabayImage called');
   // Replace commas and blank spaces
   let regExp = /[^a-z]+/gi; // Non characters like blanks commas
   let searchText = input.replace(regExp, '+');
-  console.log(`Pixabay server SearchText ${searchText}`);
   let pixabayURL = `${pixabayBaseURL}?key=${process.env.PIXABAY_API_KEY}&q=${searchText}&image_type=photo`;
-  console.log('Searching pixabay @',pixabayURL);
   return fetch(pixabayURL);
 }
 
