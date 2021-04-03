04d419f557c02f3f9ae76f8a7469543598ffb189
jamesjbot <jongs.j@gmail.com>
2021-04-03 01:05:36 -0400

    chore; Remove old test code

--- __test__/buttonpress.spec.js
+++ __test__/buttonpress.spec.js
@@ -34,21 +34,3 @@ describe("buttonpress datelogic tests", () => {
 	});
 
 });
-
-// describe("Form handler tests", () => {
-//
-// 	test("Check handleSubmit function", () => {
-// 		expect(handleSubmit).toBeDefined();
-// 	});
-//
-// 	test("Testing the handleSubmit() function", () => {
-// 		expect(validateEntry("http://www.google.com")).toBeDefined();
-// 	});
-//
-// 	test("Valid URL Checker",() => {
-// 		expect(validateEntry("http://www.google.com")).toBeTruthy();
-// 		expect(validateEntry("https://www.yahoo.com")).toBeTruthy();
-// 		expect(validateEntry("blahblahblah")).toBeFalsy();
-// 	});
-//
-// });
