79e545b7fdb5e836680250499a428d4e83533da1
jamesjbot <jongs.j@gmail.com>
2021-05-01 13:20:11 -0400

    fix: Center content in figure

--- __test__/server.spec.js
+++ __test__/server.spec.js
@@ -11,29 +11,12 @@ import 'regenerator-runtime/runtime';
 const request = require('supertest');
 const app = require('../src/server/server');
 
-describe('Post Endpoints', () => {
-  it('Testing test route', async () => {
+describe('Post User Data Endpoint', () => {
+  it('Testing Post User Data route', async () => {
+    let userdata = {place: 'NY, NY' };
     const res = await request(app)
-      .get('/test')
-      .expect( data => {
-        console.log('Hi I am a test');
-        console.log('type:',data.type);
-      })
-      .expect(200)
-      //.expect('hello world')
-      .then(response => {
-        console.log('-->body:', response.body);
-        return response;
-      })
-      .then(response => {
-        console.log('text:',response.text);
-        console.log('header:',response.header);
-      });
-    //console.log('json:',res.body.json());
-      //.expect(res.statusCode).toEqual(200);
-    //console.log('body:',res.body);
-    //console.log('status:',res.status);
-    //expect(res.body).toHaveProperty('hello');
-    //expect(res.body).toBe('hello');
+      .post('/postUserData')
+      .send(userdata)
+      .expect(200);
   });
 });
