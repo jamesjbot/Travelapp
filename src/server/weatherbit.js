782ef420c025f17b456fe3d72f1fcd5232fb69c7
jamesjbot <jongs.j@gmail.com>
2021-05-28 14:13:02 -0400

    fix: Pull the correct date's weather from the array of weatherbit forecasts.

--- src/server/weatherbit.js
+++ src/server/weatherbit.js
@@ -14,6 +14,7 @@ const weatherbitClimateNormalsDailyURL = 'https://api.weatherbit.io/v2.0/normals
 
 const regularExpression = new RegExp('/[^A-Za-z]+/');
 
+
 function retrieveWeatherDataFromWeatherBit(
   lat,
   lon,
@@ -26,45 +27,31 @@ function retrieveWeatherDataFromWeatherBit(
   return fetch(constructWeatherbitURL(lat, lon, weatherType, month_day, key))
     .then(response => response.json())
     .then(json => {
-      // TODO Find the correct weather
-      console.log('Repeating The apikey is', key);
-      console.log('We need to pick the correct forecast weather');
-      console.log('weather', json);
-      // There are only 0-15 forecast dates in the array
-      // At 6pm ish everyday the 0th day incremented to the next day.
-      // TODO this needs a date check
-      // The key for checking the date is datetime: '2021-05-24
-      // THis is the passed date '2021-06-08T04:00:00.000Z'
-      console.log(`TravelDate received by server: ${travelDate}`);
-      console.log('weatherbit\'s format is 2021-05-24');
-      let justTravelDate = travelDate.substring(0, 10);
-      console.log('Extracted Traveldate', justTravelDate);
 
-      // TODO Looks Ugly fix!
+ 
 
       if (weatherType == CURRENT) {
+      
         return {
           temp: json.data[0].temp,
           description: json.data[0].weather.description,
           icon: json.data[0].weather.icon
         };
+      
       } else if (weatherType == FORECAST) {
         
-        console.log('the length of the weatherdata array is:', json.data.length);
-        console.log(`travelDate: ${justTravelDate} valid_date: ${json.data[daysAway].valid_date}`);
-        
+        // There are only 0-15 forecast dates in the array
+        let exactTravelDate = travelDate.substring(0, 10);
         if (daysAway == 17 ) throw new ('Date is too far in the future to test array only has 16 elements daysAway:',daysAway);
-        if (justTravelDate == json.data[daysAway].valid_date) { // There server is on UTC Time 
-          console.log('---->!!!!!!!taking current entry!!!!!!!<-------');
-          return {
+        if (exactTravelDate == json.data[daysAway].valid_date) { // There server is on UTC Time 
+          return { // Use current entry
             hitemp: json.data[daysAway].high_temp,
             lowtemp: json.data[daysAway].low_temp,
             description: json.data[daysAway].weather.description,
             icon: json.data[daysAway].weather.icon
           };
         }
-        console.log('taking previous entry');
-        return {
+        return { // Use previous entry
           hitemp: json.data[daysAway - 1].high_temp,
           lowtemp: json.data[daysAway - 1].low_temp,
           description: json.data[daysAway - 1].weather.description,
@@ -82,25 +69,19 @@ function retrieveWeatherDataFromWeatherBit(
     });
 }
 
+
 function constructWeatherbitURL(lat, lon, weatherType, month_day, key) {
   console.log('constructWeatherbitURL', key);
   console.log('weatherType', weatherType)
   if (weatherType == CURRENT) {
-    //console.log(`${weatherbitCurrentURL}&lat=${lat}&lon=${lon}&key=${key}`);
     return `${weatherbitCurrentURL}&lat=${lat}&lon=${lon}&key=${key}`;
   } else if (weatherType == FORECAST) {
-    //console.log(`${weatherbitForecastURL}&lat=${lat}&lon=${lon}&key=${key}`);
     return `${weatherbitForecastURL}&lat=${lat}&lon=${lon}&key=${key}`;
   } else if (weatherType == CLIMATE) {
     throw 'Weatherbit climate unsupported';
-    //let start_date = `${minTwoDigits((date.getMonth() + 1))}-${minTwoDigits(date.getDate())}`;
-    //let start_date = `${minTwoDigits((month_day.month + 1))}-${minTwoDigits(month_day.day)}`;
-    //console.log(`${weatherbitClimateNormalsDailyURL}&start_day=${start_date}
-    //&end_day=${start_date}&lat=${lat}&lon=${lon}&key=${key}`);
-    //return `${weatherbitClimateNormalsDailyURL}&start_day=${start_date}
-    //&end_day=${start_date}&lat=${lat}&lon=${lon}&key=${key}`;
   }
 }
+
 
 function minTwoDigits(n) {
   return (n < 10 ? '0' : '') + n;
